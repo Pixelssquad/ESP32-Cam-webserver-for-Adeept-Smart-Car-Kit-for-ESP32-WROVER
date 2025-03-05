a3488464a40198683c333b00a658831022db56e9
Diegooz <85436549+Pixelssquad@users.noreply.github.com>
2025-03-05 23:22:43 +0100

    Update Adeept_Car_CameraWebServer.ino

--- Adeept_Car_CameraWebServer/Adeept_Car_CameraWebServer.ino
+++ Adeept_Car_CameraWebServer/Adeept_Car_CameraWebServer.ino
@@ -15,8 +15,8 @@ SSD1306Wire OLED_display(0x3c, SDA, SCL);
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char *ssid = "your ssid";
-const char *password = "your password";
+const char *ssid = "ssid here";
+const char *password = "password here";
 
 void startCameraServer();
 void setupLedFlash(int pin);
